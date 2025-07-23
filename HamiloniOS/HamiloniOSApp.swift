56508214345c3b0f8cfef6569c80508f4a64de79
HanSeooooL <fnsonle@gmail.com>
2025-07-23 17:42:04 +0900

    Initial Commit

---
+++ HamiloniOS/HamiloniOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamiloniOSApp.swift
+//  HamiloniOS
+//
+//  Created by 한설 on 2025/07/23.
+//
+
+import SwiftUI
+
+@main
+struct HamiloniOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
